d2247a0887d46184a7b9175b3c78bdb4948a583c
MhdFarhan011 <mohammedfarhan2806@gmail.com>
2026-08-31 18:24:57 +0530

    Update netflix_app.py

--- netflix_app.py
+++ netflix_app.py
@@ -37,7 +37,7 @@ YELLOW = "#F5C518"
 # FILE PATHS
 # =========================================================
 
-MODEL_PATH = "model.pkl"
+MODEL_PATH = "netflix_user_behaviour_model.pkl"
 SCALER_PATH = "scaler.pkl"
 DATA_PATH = "netflix_user_behavior_churn_50000.csv"
 
